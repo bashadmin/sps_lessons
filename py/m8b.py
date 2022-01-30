efc54894e21f4ada8d5ea677b4e32d1276e4c4e0
bashadmin <59800291+bashadmin@users.noreply.github.com>
2022-01-29 19:07:34 -0500

    m8b.py
    input didn't work in ca ide

---
+++ py/m8b.py
@@ -0,0 +1,43 @@
+import random
+from builtins import input
+
+name = "John"
+question = 'Do you think this is a good lab?'
+answer = ""
+#name = ""
+#question = ""
+random_number = random.randint(1,9)
+#print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "Very very very doubtful."
+elif random_number == 11:
+  answer = "Might be doubtful, I am not sure..."
+else:
+  answer = "Error"
+
+if name == "":
+  name = "Question"
+if question == "":
+  print("Please enter a question!!!!\nStart again.")
+else:
+  print(f"{name} asks: {question}")
+  print(f"Magic 8-Ball's answer: {answer}")
